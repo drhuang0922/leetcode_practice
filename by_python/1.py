727f59eb6d24cee8866be8662e34fb1680bb0d6e
doryhuang <drhuang0922@gmail.com>
2022-12-07 20:20:25 +0800

    add two sum

---
+++ by_python/1.py
@@ -0,0 +1,10 @@
+def twoSum(nums, target):
+
+    hashmap = {}
+
+    for i, num in enumerate(nums):
+        if target - num in hashmap:
+            return [hashmap[target - num], i]
+        hashmap[num] = i
+
+    return []
